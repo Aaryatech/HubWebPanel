a5ba2c7c9c934e91541cce09336926cbe92941f9
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2018-08-24 11:52:44 +0530

    // Neha

---
+++ src/main/java/com/ats/hub/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.ats.hub.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class OrderController {
+
+	RestTemplate rest = new RestTemplate();
+
+	@RequestMapping(value = "/showOrderHistory", method = RequestMethod.GET)
+	public ModelAndView showOrderHistory(HttpServletRequest request, HttpServletResponse response) {
+
+		ModelAndView model = new ModelAndView("order/orderHistory");
+		try {
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return model;
+	}
+
+}
